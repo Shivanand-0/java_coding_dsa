8a6072cfc9ea7ab9d6d8597e8c6bf47b5c8921d5
Shivanand Gupta <142976206+Shivanand-0@users.noreply.github.com>
2026-02-27 16:46:22 +0000

    Sync LeetCode submission - Sort Colors (java)

--- solution/problems/sort_colors/solution.java
+++ solution/problems/sort_colors/solution.java
@@ -1,15 +1,27 @@
 class Solution {
     public void sortColors(int[] nums) {
         int n=nums.length;
-        int temp=0;
-        for(int i=0;i<n-1;i++){
-            for(int j=i+1;j<n;j++){
-                if(nums[j]<nums[i]){
-                    temp=nums[j];
-                    nums[j]=nums[i];
-                    nums[i]=temp;
-                }
+        int low=0,mid=0;
+        int high=n-1;
+
+        while(mid<=high){
+            if(nums[mid]==0){
+                int temp=nums[low];
+                nums[low]=nums[mid];
+                nums[mid]=temp;
+                low++;
+                mid++;
+            }
+            else if(nums[mid]==1){
+                mid++;
+            }
+            else if(nums[mid]==2){
+                int temp=nums[mid];
+                nums[mid]=nums[high];
+                nums[high]=temp;
+                high--;
             }
         }
     }
+    
 }
