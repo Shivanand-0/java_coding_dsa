5fcc0898f6c9218d146c4b393de780fa77722433
Shivanand Gupta <142976206+Shivanand-0@users.noreply.github.com>
2026-02-27 10:37:26 +0000

    Sync LeetCode submission - To Lower Case (java)

--- solution/problems/to_lower_case/solution.java
+++ solution/problems/to_lower_case/solution.java
@@ -1,5 +1,16 @@
 class Solution {
     public String toLowerCase(String s) {
-        return s.toLowerCase();
+        // return s.toLowerCase();
+        // or
+        StringBuilder sb =new StringBuilder();
+        char c;
+        for(int i=0;i<s.length();i++){
+            c = s.charAt(i);
+
+            if(c>=65 && c<=90 ){
+                sb.append((char)(c+32));
+            }else sb.append(c);
+        }
+        return sb.toString();
     }
 }
